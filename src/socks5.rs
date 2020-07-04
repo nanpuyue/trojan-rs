d7b18840134559252f1dbdfd4c7d2fdecb7cac3d
南浦月 <nanpuyue@gmail.com>
2020-07-05 00:59:47 +0800

    refactor(socks5): improve error handling

--- src/socks5.rs
+++ src/socks5.rs
@@ -12,7 +12,13 @@ pub struct Socks5Listener {
     listener: TcpListener,
 }
 
-pub enum Socks5Addr {
+struct Socks5Stream {
+    stream: TcpStream,
+    buf: Vec<u8>,
+    addr: Option<Socks5Addr>,
+}
+
+enum Socks5Addr {
     V4(SocketAddrV4),
     V6(SocketAddrV6),
     Domain(String),
@@ -55,11 +61,114 @@ impl Socks5Addr {
         Ok(Self::Domain(domain + ":" + &port))
     }
 
-    pub async fn connect(&self) -> Result<TcpStream> {
+    async fn connect(&self) -> Result<TcpStream> {
         match self {
             Self::V4(s) => TcpStream::connect(s).await,
             Self::V6(s) => TcpStream::connect(s).await,
             Self::Domain(s) => TcpStream::connect(s).await,
+        }
+    }
+}
+
+impl Socks5Stream {
+    fn new(stream: TcpStream) -> Self {
+        Socks5Stream {
+            stream,
+            buf: Vec::with_capacity(512),
+            addr: None,
+        }
+    }
+
+    async fn authenticate(&mut self) -> Result<()> {
+        let mut n;
+        let mut len = 0;
+
+        self.buf.clear();
+        while {
+            n = self.stream.read_buf(&mut self.buf).await?;
+            n > 0
+        } {
+            if self.buf.len() >= 2 {
+                len = 2 + self.buf[1] as usize;
+            }
+
+            if len > 0 && self.buf.len() >= len {
+                break;
+            }
+        }
+        if len == 0 || self.buf.len() != len || self.buf[0] != 5 {
+            return Err(io_error("Invalid Request!"));
+        }
+
+        if !self.buf[2..].contains(&0) {
+            self.stream.write_all(b"\x05\xff").await?;
+            return Err(io_error("No Supported Authentication Method!"));
+        }
+
+        self.stream.write_all(b"\x05\x00").await?;
+        Ok(())
+    }
+
+    async fn handle_command(mut self) -> Result<()> {
+        let mut n;
+        let mut len = 0;
+
+        self.buf.clear();
+        while {
+            n = self.stream.read_buf(&mut self.buf).await?;
+            n > 0
+        } {
+            if len == 0 && self.buf.len() >= 5 {
+                len = match self.buf[3] {
+                    1 => 10,
+                    4 => 22,
+                    3 => 7 + self.buf[4] as usize,
+                    _ => {
+                        self.stream.write_all(b"\x05\x08").await?;
+                        return Err(io_error("Invalid Address Type!"));
+                    }
+                };
+            }
+
+            if len > 0 && self.buf.len() >= len {
+                break;
+            }
+        }
+
+        if len == 0 || self.buf.len() != len || self.buf[0] != 5 || self.buf[2] != 0 {
+            return Err(io_error("Invalid Request!"));
+        }
+
+        if self.buf[1] != 1 {
+            self.stream.write_all(b"\x05\x07").await?;
+            return Err(io_error("Unsupported Request Command!"));
+        }
+
+        self.addr = Some(match self.buf[3] {
+            1 => unsafe { Socks5Addr::parse_ipv4(&self.buf[4..]) },
+            4 => unsafe { Socks5Addr::parse_ipv6(&self.buf[4..]) },
+            3 => Socks5Addr::try_parse_domain(&self.buf[4..])?,
+            _ => unreachable!(),
+        });
+
+        eprintln!("connect {}...", self.addr.as_ref().unwrap());
+        self.handle_connect().await
+    }
+
+    async fn handle_connect(mut self) -> Result<()> {
+        match self.addr.as_ref().unwrap().connect().await {
+            Ok(upstream) => {
+                self.stream
+                    .write_all(b"\x05\x00\x00\x01\x00\x00\x00\x00\x00\x00")
+                    .await?;
+                link_tcpstream(self.stream, upstream).await
+            }
+            Err(e) => {
+                self.stream
+                    .write_all(&[b"\x05\x01\x00", &self.buf[3..]].concat())
+                    .await?;
+                Err(e)
+            }
         }
     }
 }
@@ -72,92 +181,25 @@ impl Socks5Listener {
     }
 
     pub async fn handle_incoming(&mut self) -> Result<()> {
-        async move {
+        async {
             let mut incoming = self.listener.incoming();
-            while let Some(s) = incoming.next().await.transpose()? {
-                eprintln!("accepted connection from {}", s.peer_addr().unwrap());
-                tokio::spawn(async move {
-                    if let Err(e) = Self::handle_accept(s).await {
-                        eprintln!("handle_accept err: {}", e)
+            while let Some(stream) = incoming.next().await.transpose()? {
+                eprintln!("accepted connection from {}", stream.peer_addr().unwrap());
+                tokio::spawn(async {
+                    if let Err(e) = async {
+                        let mut socks5_stream = Socks5Stream::new(stream);
+                        socks5_stream.authenticate().await?;
+                        socks5_stream.handle_command().await
+                    }
+                    .await
+                    {
+                        eprintln!("handle incoming err: {}", e)
                     }
                 });
             }
             Ok(())
         }
         .await
-    }
-
-    async fn handle_accept(mut c: TcpStream) -> Result<()> {
-        let mut buf = Vec::with_capacity(512);
-        let mut n;
-        let mut len = 0;
-
-        while {
-            n = c.read_buf(&mut buf).await?;
-            n > 0
-        } {
-            if buf.len() >= 2 {
-                len = 2 + buf[1] as usize;
-            }
-
-            if len > 0 && buf.len() >= len {
-                break;
-            }
-        }
-        if len == 0 || buf.len() != len || buf[0] != 5 {
-            return Err(io_error("Invalid Request!"));
-        }
-
-        if !buf[2..].contains(&0) {
-            c.write_all(b"\x05\xff").await?;
-            return Err(io_error("No Supported Authentication Method!"));
-        }
-
-        c.write_all(b"\x05\x00").await?;
-
-        buf.clear();
-        len = 0;
-
-        while {
-            n = c.read_buf(&mut buf).await?;
-            n > 0
-        } {
-            if len == 0 && buf.len() >= 5 {
-                len = match buf[3] {
-                    1 => 10,
-                    4 => 22,
-                    3 => 7 + buf[4] as usize,
-                    _ => return Err(io_error("Invalid Address Type!")),
-                };
-            }
-
-            if len > 0 && buf.len() >= len {
-                break;
-            }
-        }
-
-        if len == 0 || buf.len() != len || buf[0] != 5 || buf[2] != 0 {
-            return Err(io_error("Invalid Request!"));
-        }
-
-        if buf[1] != 1 {
-            return Err(io_error("Unsupported Request Command!"));
-        }
-
-        let addr = match buf[3] {
-            1 => unsafe { Socks5Addr::parse_ipv4(&buf[4..]) },
-            4 => unsafe { Socks5Addr::parse_ipv6(&buf[4..]) },
-            3 => Socks5Addr::try_parse_domain(&buf[4..])?,
-            _ => unreachable!(),
-        };
-
-        eprintln!("connect {}...", addr);
-        let s = addr.connect().await?;
-        // TODO
-        c.write_all(b"\x05\x00\x00\x01\x00\x00\x00\x00\x00\x00")
-            .await?;
-
-        link_tcpstream(c, s).await
     }
 }
 
