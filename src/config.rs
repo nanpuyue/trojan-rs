2596342ea7a84579496920eae32397b850cb62ad
南浦月 <nanpuyue@gmail.com>
2020-07-12 02:14:55 +0800

    refactor(config): make all fields public

--- src/config.rs
+++ src/config.rs
@@ -4,47 +4,47 @@ pub use self::ssl::Config as SslConfig;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
-    run_type: String,
-    local_addr: String,
-    local_port: u16,
-    remote_addr: String,
-    remote_port: u16,
+    pub run_type: String,
+    pub local_addr: String,
+    pub local_port: u16,
+    pub remote_addr: String,
+    pub remote_port: u16,
     #[serde(skip_serializing_if = "Option::is_none")]
-    target_addr: Option<String>,
+    pub target_addr: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    target_port: Option<u16>,
-    password: Vec<String>,
+    pub target_port: Option<u16>,
+    pub password: Vec<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    udp_timeout: Option<u32>,
-    log_level: u8,
-    ssl: SslConfig,
-    tcp: TcpConfig,
+    pub udp_timeout: Option<u32>,
+    pub log_level: u8,
+    pub ssl: SslConfig,
+    pub tcp: TcpConfig,
     #[serde(skip_serializing_if = "Option::is_none")]
-    mysql: Option<MysqlConfig>,
+    pub mysql: Option<MysqlConfig>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct TcpConfig {
     #[serde(skip_serializing_if = "Option::is_none")]
-    prefer_ipv4: Option<bool>,
-    no_delay: bool,
-    keep_alive: bool,
-    reuse_port: bool,
-    fast_open: bool,
-    fast_open_qlen: u32,
+    pub prefer_ipv4: Option<bool>,
+    pub no_delay: bool,
+    pub keep_alive: bool,
+    pub reuse_port: bool,
+    pub fast_open: bool,
+    pub fast_open_qlen: u32,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MysqlConfig {
-    enabled: bool,
-    server_addr: String,
-    server_port: u16,
-    database: String,
-    username: String,
-    password: String,
-    key: String,
-    cert: String,
-    ca: String,
+    pub enabled: bool,
+    pub server_addr: String,
+    pub server_port: u16,
+    pub database: String,
+    pub username: String,
+    pub password: String,
+    pub key: String,
+    pub cert: String,
+    pub ca: String,
 }
 
 mod ssl {
@@ -54,34 +54,34 @@ mod ssl {
 
     #[derive(Debug, Deserialize, Serialize)]
     pub struct Client {
-        verify: bool,
-        verify_hostname: bool,
-        cert: String,
-        cipher: String,
-        cipher_tls13: String,
-        sni: String,
-        alpn: Vec<String>,
-        reuse_session: bool,
-        session_ticket: bool,
-        curves: String,
+        pub verify: bool,
+        pub verify_hostname: bool,
+        pub cert: String,
+        pub cipher: String,
+        pub cipher_tls13: String,
+        pub sni: String,
+        pub alpn: Vec<String>,
+        pub reuse_session: bool,
+        pub session_ticket: bool,
+        pub curves: String,
     }
 
     #[derive(Debug, Deserialize, Serialize)]
     pub struct Server {
-        cert: String,
-        key: String,
-        key_password: String,
-        cipher: String,
-        cipher_tls13: String,
-        prefer_server_cipher: bool,
-        alpn: Vec<String>,
-        alpn_port_override: HashMap<String, u16>,
-        reuse_session: bool,
-        session_ticket: bool,
-        session_timeout: u32,
-        plain_http_response: String,
-        curves: String,
-        dhparam: String,
+        pub cert: String,
+        pub key: String,
+        pub key_password: String,
+        pub cipher: String,
+        pub cipher_tls13: String,
+        pub prefer_server_cipher: bool,
+        pub alpn: Vec<String>,
+        pub alpn_port_override: HashMap<String, u16>,
+        pub reuse_session: bool,
+        pub session_ticket: bool,
+        pub session_timeout: u32,
+        pub plain_http_response: String,
+        pub curves: String,
+        pub dhparam: String,
     }
 
     #[serde(untagged)]
