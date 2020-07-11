58ce2ff80d26d53235e276961af2e5bfc804f752
南浦月 <nanpuyue@gmail.com>
2020-07-11 23:25:04 +0800

    refactor(socks5): rewrite `socks5.rs`
    * add `TargetConnector` & `DirectConnector`
    * `handle_connect` with `TargetConnector`
    * rename `Socks5Addr` to `Socks5Target`

--- src/socks5.rs
+++ src/socks5.rs
@@ -1,12 +1,26 @@
 use std::fmt::{Display, Formatter};
 use std::net::{SocketAddrV4, SocketAddrV6};
 
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use async_trait::async_trait;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream, ToSocketAddrs};
 use tokio::stream::StreamExt;
 
 use crate::error::Result;
 use crate::util::link_stream;
+
+#[async_trait]
+pub trait TargetConnector: Send {
+    type Stream: AsyncRead + AsyncWrite;
+
+    async fn connect(&mut self) -> Result<()>;
+
+    async fn send_request(&mut self) -> Result<()> {
+        Ok(())
+    }
+
+    fn take_stream(&mut self) -> Option<Self::Stream>;
+}
 
 pub struct Socks5Listener {
     listener: TcpListener,
@@ -15,16 +29,30 @@ pub struct Socks5Listener {
 struct Socks5Stream {
     stream: TcpStream,
     buf: Vec<u8>,
-    addr: Option<Socks5Addr>,
+    target: Vec<u8>,
 }
 
-enum Socks5Addr {
+pub enum Socks5Target {
     V4(SocketAddrV4),
     V6(SocketAddrV6),
     Domain(String),
 }
 
-impl Display for Socks5Addr {
+pub struct DirectConnector {
+    target: Socks5Target,
+    stream: Option<TcpStream>,
+}
+
+impl From<Socks5Target> for DirectConnector {
+    fn from(target: Socks5Target) -> Self {
+        Self {
+            target,
+            stream: None,
+        }
+    }
+}
+
+impl Display for Socks5Target {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::V4(s) => s.fmt(f),
@@ -34,7 +62,7 @@ impl Display for Socks5Addr {
     }
 }
 
-impl Socks5Addr {
+impl Socks5Target {
     unsafe fn parse_ipv4(data: &[u8]) -> Self {
         Self::V4(SocketAddrV4::new(
             (*(data.as_ptr() as *const [u8; 4])).into(),
@@ -60,13 +88,24 @@ impl Socks5Addr {
         let port = u16::from_be_bytes([data[len - 2], data[len - 1]]).to_string();
         Ok(Self::Domain(domain + ":" + &port))
     }
+}
 
-    async fn connect(&self) -> Result<TcpStream> {
-        Ok(match self {
-            Self::V4(s) => TcpStream::connect(s).await?,
-            Self::V6(s) => TcpStream::connect(s).await?,
-            Self::Domain(s) => TcpStream::connect(s).await?,
-        })
+#[async_trait]
+impl TargetConnector for DirectConnector {
+    type Stream = TcpStream;
+
+    async fn connect(&mut self) -> Result<()> {
+        self.stream = Some(match &self.target {
+            Socks5Target::V4(s) => TcpStream::connect(s).await?,
+            Socks5Target::V6(s) => TcpStream::connect(s).await?,
+            Socks5Target::Domain(s) => TcpStream::connect(s).await?,
+        });
+
+        Ok(())
+    }
+
+    fn take_stream(&mut self) -> Option<Self::Stream> {
+        self.stream.take()
     }
 }
 
@@ -75,7 +114,7 @@ impl Socks5Stream {
         Socks5Stream {
             stream,
             buf: Vec::with_capacity(512),
-            addr: None,
+            target: Vec::new(),
         }
     }
 
@@ -107,6 +146,15 @@ impl Socks5Stream {
 
         self.stream.write_all(b"\x05\x00").await?;
         Ok(())
+    }
+
+    unsafe fn parse_target(&self) -> Result<Socks5Target> {
+        Ok(match self.target[0] {
+            1 => Socks5Target::parse_ipv4(&self.target[1..]),
+            4 => Socks5Target::parse_ipv6(&self.target[1..]),
+            3 => Socks5Target::try_parse_domain(&self.target[1..])?,
+            _ => unreachable!(),
+        })
     }
 
     async fn handle_command(mut self) -> Result<()> {
@@ -144,24 +192,23 @@ impl Socks5Stream {
             return Err("Unsupported Request Command!".into());
         }
 
-        self.addr = Some(match self.buf[3] {
-            1 => unsafe { Socks5Addr::parse_ipv4(&self.buf[4..]) },
-            4 => unsafe { Socks5Addr::parse_ipv6(&self.buf[4..]) },
-            3 => Socks5Addr::try_parse_domain(&self.buf[4..])?,
-            _ => unreachable!(),
-        });
+        self.target = self.buf.split_off(3);
 
-        eprintln!("connect {}...", self.addr.as_ref().unwrap());
-        self.handle_connect().await
+        self.handle_connect::<DirectConnector>().await
     }
 
-    async fn handle_connect(mut self) -> Result<()> {
-        match self.addr.as_ref().unwrap().connect().await {
-            Ok(upstream) => {
+    async fn handle_connect<C: TargetConnector + From<Socks5Target>>(mut self) -> Result<()> {
+        let target = unsafe { self.parse_target()? };
+        eprintln!("connect {}...", target);
+
+        let connector = &mut C::from(target);
+        match connector.connect().await {
+            Ok(_) => {
                 self.stream
                     .write_all(b"\x05\x00\x00\x01\x00\x00\x00\x00\x00\x00")
                     .await?;
-                link_stream(self.stream, upstream).await
+                connector.send_request().await?;
+                link_stream(self.stream, connector.take_stream().unwrap()).await
             }
             Err(e) => {
                 self.stream
