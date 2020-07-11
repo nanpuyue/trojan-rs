9ad584fb47c73ad1d59fb30459fb9864be3b10bf
南浦月 <nanpuyue@gmail.com>
2020-07-11 22:10:01 +0800

    feat: add `util::ToHex`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod config;
 pub mod error;
 pub mod socks5;
+pub mod util;
---
+++ src/util.rs
@@ -0,0 +1,17 @@
+pub trait ToHex {
+    fn to_hex(&self) -> String;
+}
+
+const CHARS: &[u8] = b"0123456789abcdef";
+
+impl ToHex for [u8] {
+    fn to_hex(&self) -> String {
+        let mut v = Vec::with_capacity(self.len() * 2);
+        for &b in self {
+            v.push(CHARS[(b >> 4) as usize]);
+            v.push(CHARS[(b & 0xf) as usize]);
+        }
+
+        unsafe { String::from_utf8_unchecked(v) }
+    }
+}
